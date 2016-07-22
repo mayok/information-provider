e4be1bdc3b529ce21cbee925fa743d448e91ef66
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2016-07-22 16:16:40 +0900

    いろいろ追加
    - StaticPagesController
    - User model
    - Page model

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '4.2.6'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
+gem 'slim-rails'
 
 group :development do
   gem 'web-console', '~> 2.0'
---
+++ db/migrate/20160722071614_create_pages.rb
@@ -0,0 +1,11 @@
+class CreatePages < ActiveRecord::Migration
+  def change
+    create_table :pages do |t|
+      t.string :title
+      t.text :content
+      t.text :url
+
+      t.timestamps null: false
+    end
+  end
+end
